cef669e6e2c662278cbdedf151ace5c5d48b32cd
HecGalvQ <hectorquiroz72@aragon.unam.mx>
2022-05-25 11:38:13 -0500

    Tareas Y Sesiones agregadas

---
+++ tareas/InterfacesUsuario2210/src/interfacesusuario2210/InterfacesUsuario2210.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package interfacesusuario2210;
+
+import ico.fes.iu.MiVentana;
+import ico.fes.iu.MiVentanaV2;
+import ico.fes.iu.swing.VentanaSwing;
+
+/**
+ *
+ * @author herma
+ */
+public class InterfacesUsuario2210 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //MiVentana w=new MiVentana();
+        MiVentanaV2 v2= new MiVentanaV2();
+        VentanaSwing vs= new VentanaSwing(); 
+        
+    }
+    
+}
